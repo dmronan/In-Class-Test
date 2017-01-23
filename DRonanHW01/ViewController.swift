48c5224a4a7483491a5c76c41a7ec1174b5cf579
Dom Ronan <dominickronan@doms-mbp.bc.edu>
2017-01-23 18:28:13 -0500

    Added another variable

--- DRonanHW01/ViewController.swift
+++ DRonanHW01/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var holder = Int = 0
+    var holder: Int = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
